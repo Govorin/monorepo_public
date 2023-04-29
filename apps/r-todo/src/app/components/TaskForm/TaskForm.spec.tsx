bb4cb66cdd641c95c64ce3cc96c41160176311db
govorin <govorinviktor@gmail.com>
2023-04-29 20:54:55 +0600

    feat: add animation to TaskItem component
    - Add `Slide` transition from `@mui/material` to provide animation on mount and unmount of the component
    - Use `useSpring` from `react-spring` to animate opacity and color of the component based on task completion status
    - Update `TaskItem` component to use the new animation and state handling for task completion status

--- apps/r-todo/src/app/components/TaskForm/TaskForm.spec.tsx
+++ apps/r-todo/src/app/components/TaskForm/TaskForm.spec.tsx
@@ -1,10 +1,1 @@
-import { render } from '@testing-library/react';
-
-import TaskForm from './TaskForm';
-
-describe('TaskForm', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<TaskForm />);
-    expect(baseElement).toBeTruthy();
-  });
-});
+describe('TaskForm', () => {});
--- apps/r-todo/src/app/components/TaskItem/TaskItem.tsx
+++ apps/r-todo/src/app/components/TaskItem/TaskItem.tsx
@@ -7,9 +7,12 @@ import {
   ListItemIcon,
   ListItemSecondaryAction,
   ListItemText,
+  Slide,
 } from '@mui/material';
+import { memo, useEffect, useState } from 'react';
+import { animated, useSpring } from 'react-spring';
+
 import { Task } from '@types';
-import { memo, useState } from 'react';
 
 // import styles from './TaskItem.module.scss';
 
@@ -20,49 +23,68 @@ export interface TaskItemProps {
 }
 
 const TaskItemComponent = ({ task, onTaskDeleted, onTaskChanged }: TaskItemProps) => {
+  const [isMounted, setIsMounted] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
+  const [completed, setCompleted] = useState(task.completed);
 
+  useEffect(() => {
+    setIsMounted(true);
+
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+  const props = useSpring({
+    opacity: completed ? 0.5 : 1,
+    color: completed ? '#bbb' : '#000',
+  });
   const handleToggle = async () => {
     setIsUpdating(true);
     await onTaskChanged(task.id, { completed: !task.completed });
+    setCompleted(!completed);
     setIsUpdating(false);
   };
 
   const handleDelete = async () => {
     setIsDeleting(true);
+    setIsMounted(false);
     await onTaskDeleted(task.id);
     setIsDeleting(false);
   };
 
   return (
-    <ListItem>
-      <>
-        <ListItemIcon>
-          {isUpdating ? (
-            <CircularProgress style={{ padding: '9px 0' }} size={24} />
-          ) : (
-            <Checkbox
-              edge="start"
-              checked={task.completed}
-              tabIndex={-1}
-              disableRipple
-              onClick={handleToggle}
-            />
-          )}
-        </ListItemIcon>
-        <ListItemText primary={task.title} />
-        <ListItemSecondaryAction>
-          {isDeleting ? (
-            <CircularProgress style={{ padding: '9px 0' }} size={24} />
-          ) : (
-            <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
-              <DeleteIcon />
-            </IconButton>
-          )}
-        </ListItemSecondaryAction>
-      </>
-    </ListItem>
+    <Slide appear={isMounted} in={isMounted} timeout={300} direction="left">
+      <animated.li style={props}>
+        <ListItem>
+          <>
+            <ListItemIcon>
+              {isUpdating ? (
+                <CircularProgress style={{ padding: '9px 0' }} size={24} />
+              ) : (
+                <Checkbox
+                  edge="start"
+                  checked={task.completed}
+                  tabIndex={-1}
+                  disableRipple
+                  onClick={handleToggle}
+                />
+              )}
+            </ListItemIcon>
+            <ListItemText primary={task.title} />
+            <ListItemSecondaryAction>
+              {isDeleting ? (
+                <CircularProgress style={{ padding: '9px 0' }} size={24} />
+              ) : (
+                <IconButton edge="end" aria-label="delete" onClick={handleDelete}>
+                  <DeleteIcon />
+                </IconButton>
+              )}
+            </ListItemSecondaryAction>
+          </>
+        </ListItem>
+      </animated.li>
+    </Slide>
   );
 };
 
